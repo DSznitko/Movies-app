dca2a07aa0385d8f3a77fe9f92768bebd84561c3
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2023-04-25 20:46:43 +0200

    partly styled avatar page

--- src/pages/avatar/Avatar.jsx
+++ src/pages/avatar/Avatar.jsx
@@ -6,7 +6,23 @@ const Avatar = () => {
   const { title, overview, poster_path, release_date, vote_average } =
     avatarData.results[0];
 
-  return <></>;
+  return (
+    <div className={classes.avatar__wrapper}>
+      <div className={classes.main__content}>
+        <h1 className={classes.avatar__title}>{title}</h1>
+        <img
+          className={classes.avatar__img}
+          src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+          alt={title}
+        />
+        <div className={classes.avatar__relase}>
+          <span className={classes.avatar__date}>
+            Relase date: {release_date}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Avatar;
