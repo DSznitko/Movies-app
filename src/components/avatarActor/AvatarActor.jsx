ad329a40dc37aa817ed4bf97db515dac52c9c911
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2023-04-29 17:35:21 +0200

    finish styling avatar page

--- src/components/avatarActor/AvatarActor.jsx
+++ src/components/avatarActor/AvatarActor.jsx
@@ -3,10 +3,10 @@ import classes from "./AvatarActor.module.css";
 
 const AvatarActor = (props) => {
   const { name, img } = props;
-  console.log(img);
+
   return (
     <div className={classes.actor__content}>
-      <img className={classes.actor__img} src={img} alt={name + "image"} />
+      <img className={classes.actor__img} src={img} alt={name} />
       <span className={classes.actor__name}>{name}</span>
     </div>
   );
