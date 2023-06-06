d711ee235fa51db7e72894efdba6436dc6296bcb
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2023-06-06 21:03:01 +0200

    create pagination handler functions

--- src/context/GenreMoviesContext.jsx
+++ src/context/GenreMoviesContext.jsx
@@ -6,9 +6,24 @@ export const GenreMoviesContextProvider = ({ children }) => {
   const [genreMovies, setGenreMovies] = useState([]);
   const [page, setPage] = useState(1);
 
+  const nextPageHandler = () => {
+    return setPage((prev) => prev + 1);
+  };
+
+  const prevPageHandler = () => {
+    return setPage((prev) => prev - 1);
+  };
+
   return (
     <GenreMoviesContext.Provider
-      value={{ genreMovies, setGenreMovies, page, setPage }}
+      value={{
+        genreMovies,
+        setGenreMovies,
+        page,
+        setPage,
+        nextPageHandler,
+        prevPageHandler,
+      }}
     >
       {children}
     </GenreMoviesContext.Provider>
