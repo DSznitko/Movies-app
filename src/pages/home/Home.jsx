edc2b4c295c74e18180be2e058e31f2719ea09ed
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2023-06-06 20:29:48 +0200

    add genre movies component to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../components/header/Header";
 import PopularMovies from "../../components/popularMovies/PopularMovies";
 import HeroSection from "../../components/heroSection/HeroSection";
+import GenresMovies from "../../components/genresMovies/GenresMovies";
 const Home = () => {
   return (
     <>
       <Header />
       <PopularMovies />
       <HeroSection />
+      <GenresMovies />
     </>
   );
 };
