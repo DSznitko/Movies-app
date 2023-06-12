5a9ab426d28dca3e64f0149a9594440dbf5ce59e
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2023-06-12 20:56:09 +0200

    styled genre buttons

--- src/components/genreButtons/GenreButtons.jsx
+++ src/components/genreButtons/GenreButtons.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import classes from "./GenreButtons.module.css";
 
 const GenreButtons = () => {
-  return <div>GenreButtons</div>;
+  return (
+    <>
+      <div className={classes.buttons}>
+        <button className={classes.genre__button}>All</button>
+        <button className={classes.genre__button}>Action</button>
+        <button className={classes.genre__button}>Comedy</button>
+      </div>
+    </>
+  );
 };
 
 export default GenreButtons;
