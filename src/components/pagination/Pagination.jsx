55e60f962bb170e92a2f9a833756170206b3160a
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2023-07-09 16:00:24 +0200

    add pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Pagination.module.css";
-
+import GenreMoviesContext from "../../context/GenreMoviesContext";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 const Pagination = () => {
-  return <div>Pagination</div>;
+  const { page, nextPageHandler, prevPageHandler } =
+    useContext(GenreMoviesContext);
+  return (
+    <>
+      <div className={classes.wrapper}>
+        <button
+          disabled={page === 1 ? true : false}
+          onClick={() => prevPageHandler()}
+          className={`${classes.button} ${page === 1 ? classes.disabled : ""}`}
+        >
+          <AiOutlineLeft />
+        </button>
+        <span className={classes.page}>Page: {page}</span>
+        <button onClick={() => nextPageHandler()} className={classes.button}>
+          <AiOutlineRight />
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Pagination;
