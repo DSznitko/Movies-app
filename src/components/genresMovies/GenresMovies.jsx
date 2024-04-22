2426419ed6deef61d7b1f21a97aa8aef5317d228
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2024-04-22 19:06:27 +0200

    optimize genre movies filtering

--- src/components/genresMovies/GenresMovies.jsx
+++ src/components/genresMovies/GenresMovies.jsx
@@ -1,24 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import classes from "./GenresMovies.module.css";
 import GenreButtons from "../genreButtons/GenreButtons";
 import Pagination from "../pagination/Pagination";
-import useFetchData from "../../hooks/useFetch";
 import GenreMoviesContext from "../../context/GenreMoviesContext";
 import GenreMovie from "../genreMovie/GenreMovie";
 import { motion } from "framer-motion";
 
 const GenresMovies = () => {
-  const api_key = process.env.REACT_APP_API_KEY;
-  const { setGenreMovies, filteredMovies, setFilteredMovies, page } =
-    useContext(GenreMoviesContext);
-  const { data } = useFetchData(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page}`
-  );
-
-  useEffect(() => {
-    setGenreMovies(data.results);
-    setFilteredMovies(data.results);
-  }, [setFilteredMovies, setGenreMovies, data]);
+  const { filteredMovies } = useContext(GenreMoviesContext);
 
   return (
     <>
--- src/context/GenreMoviesContext.jsx
+++ src/context/GenreMoviesContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect, useCallback } from "react";
+import useFetchData from "../hooks/useFetch";
 
 const GenreMoviesContext = createContext();
 
@@ -8,13 +9,25 @@ export const GenreMoviesContextProvider = ({ children }) => {
   const [activeGenre, setActiveGenre] = useState(0);
   const [page, setPage] = useState(1);
 
-  const nextPageHandler = () => {
-    return setPage((prev) => prev + 1);
-  };
+  const api_key = process.env.REACT_APP_API_KEY;
+  let API_CALL =
+    activeGenre === 0
+      ? `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page}`
+      : `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&with_genres=${activeGenre}&language=en-US&page=${page}`;
 
-  const prevPageHandler = () => {
+  const { data } = useFetchData(API_CALL);
+
+  useEffect(() => {
+    setFilteredMovies(data.results);
+  }, [data]);
+
+  const nextPageHandler = useCallback(() => {
+    return setPage((prev) => prev + 1);
+  }, []);
+
+  const prevPageHandler = useCallback(() => {
     return setPage((prev) => prev - 1);
-  };
+  }, []);
 
   return (
     <GenreMoviesContext.Provider
