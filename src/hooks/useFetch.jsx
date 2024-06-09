891bd4673a568e31f7ec0246301ea7eb90ec9ac4
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2024-06-09 14:23:43 +0200

    install react indicators package

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -4,18 +4,21 @@ import axios from "axios";
 const useFetchData = (url) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-
+    setLoading(true);
     axios
       .get(url, { signal })
       .then((res) => {
         setData(res.data);
+        setLoading(false);
       })
       .catch((error) => {
         setError(error.message);
+        setLoading(false);
       });
 
     // cleanup function
