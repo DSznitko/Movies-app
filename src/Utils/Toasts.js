4e9b70a6d87f68eed50c4b04c548a5abff6d6f02
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2024-11-12 21:16:06 +0100

    create warning toaso

--- src/Utils/Toasts.js
+++ src/Utils/Toasts.js
@@ -21,3 +21,25 @@ export const addMovieToast = (movieTitle) => {
     }
   );
 };
+
+export const sameMovieToast = (movieTitle) => {
+  toast.success(
+    <span>
+      <strong style={{ color: "#0b64eb" }}>{`"${movieTitle}"`}</strong> this
+      movie is already on your list
+    </span>,
+    {
+      style: {
+        border: "1px solid #0b64eb",
+        color: "#fff",
+        background: "#000000e6",
+        fontSize: "1.3rem",
+      },
+      iconTheme: {
+        primary: "#0b64eb",
+        secondary: "#000000",
+      },
+      duration: 1800,
+    }
+  );
+};
